e4ffc387d29ba74dcac0025c14192113dd577a35
Bobby Aleem <bobbybeatmaker@hotmail.com>
2023-08-24 22:34:59 +0500

    update

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,11 +1,59 @@
-import { FC } from "react";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser, fetchUsers, getActivity } from "@/lib/actions/user.actions";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+import Link from "next/link";
 
-const page: FC = () => {
+const Page = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) {
+    redirect("/onboarding");
+  }
+
+  const activity = await getActivity(userInfo._id);
+  console.log(activity, 'here')
+
   return (
     <section>
       <h1 className="head-text mb-10">Activity</h1>
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
+          <>
+            {activity.map((activity) => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article className="activity-card">
+                  <Image
+                    src={activity.author?.image}
+                    alt="profile picture"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">{activity.author.name}</span>{" "}replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet</p>
+        )}
+      </section>
     </section>
   );
 };
 
-export default page;
+export default Page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -138,3 +138,27 @@ export async function fetchUsers({
     throw new Error(`Failed to fetch users ${error.message}`);
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    const userThreads = await Thread.find({ author: userId });
+
+    const childThreadsIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    },[]);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadsIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image_id",
+    });
+    return replies;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch activity ${error.message}`);
+  }
+}
